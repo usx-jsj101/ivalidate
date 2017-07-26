498203f26b124b19d2e581fe7bc5ed0b57389eb9
wuhuihao <wuhuihao@raycloud.com>
2017-07-26 23:40:59 +0800

    update

--- src/core.js
+++ src/core.js
@@ -7,31 +7,101 @@
         }
 
     function validate(targetSelector, userOption) {
+
         if(typeof(targetSelector)=="string"){
             targetSelector=document.querySelector(targetSelector);
         }
         return new Core(targetSelector,$.extend(true, defaultOption, userOption));
     }
 
-    tool.getFromElementList=function(targetForm){
+    tool.getFormElementList=function(targetForm){
+	
         var list=[];
-        console.log(targetForm.elements.length);
         if(targetForm.tagName.toLowerCase()=="form"){
             for (var i = targetForm.elements.length - 1; i >= 0; i--) {
-                console.log(targetForm.elements[i]);
+                list.push(targetForm.elements[i]);  
             }
         }
 
         return list;
     }
+	
+	function FormElement(targetFormElement){
+		var element={};
+		
+		this.input=targetFormElement;
+		this.tagName=this.input.tagName;
+		
+		if(this.tagName=='textarea'){
+			
+			this.type="textarea";	
+			element=new inputElement();
+		}else if(this.tagName=="select"){
+			
+			this.type="select";
+			element=new selectElement();
+		}else{
+			
+            this.type=this.input.type;
+			switch(this.input.type){
 
+				case "text" :element = new inputElement();
+							 break;
+				case "checkbox":element = new checkboxElement();
+								break;
+				case "radio":element = new radioElement();
+							 break;
+			    default :element=false;
+			}
+		}
+		this.element=element;
+
+	}
+
+	FormElement.prototype={
+		getType:function()
+			{
+				return this.type;
+			}
+	}
+
+	function inputElement(){
+
+	}
+
+	inputElement.prototype=new FormElement();
+
+	function textareaElement(){
+
+	}
+	
+    textareaElement.prototype=new FormElement();
+
+
+	function selectElement(){
+	}
+	
+	function checkboxElement(){
+	}
+
+	function radioElement(){
+	}
+
+	
     function Core(targetForm,userOption){
+
         this.option=userOption || {};
         this.form=targetForm;
+		this.elements=tool.getFormElementList(targetForm);
     }
 
+	Core.prototype.validate=function(){
+		
+	}
+
     Core.prototype.auto=function(){
-        tool.getFromElementList(this.form);
+
+        tool.getFormElementList(this.form);
     };
 
     Core.prototype.exec=function(){
